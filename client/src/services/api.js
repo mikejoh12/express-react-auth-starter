4c36747ca56d08777d0fb82ea64f4b9568d6f254
Michael Johansson <mikejoh12@gmail.com>
2022-01-17 22:02:31 -0600

    log out UI when session expires on 401 response

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,25 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { logOut } from '../features/auth/authSlice';
+import { showSnackbar } from '../features/ui/uiSlice';
+
+
+const baseQuery = fetchBaseQuery({ baseUrl: '/api' })
+
+const baseQueryWithLogout = async (args, api, extraOptions) => {
+  let result = await baseQuery(args, api, extraOptions)
+  if (result.error && result.error.status === 401) {
+    // Session expired - log out the UI
+    api.dispatch(logOut()) // Clear Redux store
+    api.dispatch(showSnackbar({
+      message: 'Session has expired. Please log back in.',
+      severity: 'success'
+    }));
+  }
+  return result
+}
 
 export const api = createApi({
-    baseQuery: fetchBaseQuery({baseUrl: '/api'}),
+    baseQuery: baseQueryWithLogout,
     endpoints: (builder) => ({
       login: builder.mutation({
         query: (credentials) => ({
--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,9 @@ function checkAuth(req,res,next){
     if(req.isAuthenticated()){
         next();
     } else{
-        res.status(401).send("You are not authorized to access this route.");
+        res.status(401).json(
+            { error: { status: 401, data: 'Not authorized' }}
+            );
     }
 }
 
